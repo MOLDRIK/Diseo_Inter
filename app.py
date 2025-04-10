090c2af5bb72645a6d20b192a33a60d4e72e5f61
MOLDRIK <candres15@hotmail.com>
2025-04-10 16:32:14 -0500

    confirmar

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/notas")
+def notas():
+    return render_template("notas.html")
+
+if __name__==("__main__"):
+    app.run(host="0.0.0.0", port=3000, debug=True)
